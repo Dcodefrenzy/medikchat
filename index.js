848178af7c335a55e8a4057bcdbf5d7fdb5ee748
Dcodefrenzy <ayodejisamuelfakunle@gmail.com>
2020-02-09 20:15:13 -0800

    cht push

--- index.js
+++ index.js
@@ -136,6 +136,6 @@ io.on('connection', function(socket){
     
   });
 
-  http.listen(8080, function(){
-    console.log('listening on *:8080');
+  http.listen(7979, function(){
+    console.log('listening on *:7979');
   });
